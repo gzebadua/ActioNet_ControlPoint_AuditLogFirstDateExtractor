450989a4f9ad93dc26679be231bb2d34d280d5ec
Guillermo Zebadua <gzebadua@gmail.com>
2015-08-18 09:56:01 -0400

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ControlPoint_AuditLogFirstDateExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDays = new System.Windows.Forms.Label();
+            this.lblDaysQty = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lstSiteCollections = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDays
+            // 
+            this.lblDays.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDays.Location = new System.Drawing.Point(290, 25);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(208, 59);
+            this.lblDays.TabIndex = 0;
+            this.lblDays.Text = "1. Select the sites collections you want to scan to retrieve the number of days o" +
+    "f audit data (otherwise all site collections will be indexed)";
+            // 
+            // lblDaysQty
+            // 
+            this.lblDaysQty.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDaysQty.AutoSize = true;
+            this.lblDaysQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDaysQty.Location = new System.Drawing.Point(301, 204);
+            this.lblDaysQty.Name = "lblDaysQty";
+            this.lblDaysQty.Size = new System.Drawing.Size(192, 13);
+            this.lblDaysQty.TabIndex = 0;
+            this.lblDaysQty.Text = "Click the button to start indexing";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(346, 104);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(118, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "2. Click Here";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(505, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(150, 17);
+            this.toolStripStatusLabel1.Text = "Retrieving site collections...";
+            // 
+            // lstSiteCollections
+            // 
+            this.lstSiteCollections.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstSiteCollections.FormattingEnabled = true;
+            this.lstSiteCollections.Location = new System.Drawing.Point(12, 12);
+            this.lstSiteCollections.Name = "lstSiteCollections";
+            this.lstSiteCollections.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstSiteCollections.Size = new System.Drawing.Size(272, 264);
+            this.lstSiteCollections.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Final Day Count:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSiteCollections);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDaysQty);
+            this.Controls.Add(this.lblDays);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Audit Log Data Days Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.Label lblDaysQty;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox lstSiteCollections;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace ControlPoint_AuditLogFirstDateExtractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeDaysQtyTextInAnotherThread(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(ChangeDaysQtyTextInAnotherThread), new object[] { value });
+                return;
+            }
+            lblDaysQty.Text = value;
+        }
+
+        public void ChangeToolStripTextInAnotherThread(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(ChangeToolStripTextInAnotherThread), new object[] { value });
+                return;
+            }
+            toolStripStatusLabel1.Text = value;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //new Thread(startIndexing).Start();
+            startIndexing();
+        }
+
+        private void startIndexing()
+        {
+
+            ChangeToolStripTextInAnotherThread("Indexing...");
+
+            DateTime minDate = DateTime.Now;
+            System.TimeSpan diffResult;
+
+            SPSecurity.RunWithElevatedPrivileges(delegate
+            {
+
+                for (int i = 1; i <= lstSiteCollections.SelectedItems.Count; i++)
+                {
+
+                    using (SPSite siteCollection = new SPSite(lstSiteCollections.SelectedItems[i - 1].ToString()))
+                    {
+                        SPAuditQuery wssQuery = new SPAuditQuery(siteCollection);
+                        wssQuery.RowLimit = 10000;
+                        SPAuditEntryCollection auditCol = siteCollection.Audit.GetEntries(wssQuery);
+                        
+                        foreach (SPAuditEntry entry in auditCol)
+                        {
+                            if (entry.Occurred < minDate)
+                            {
+                                minDate = entry.Occurred;
+                                diffResult = DateTime.Now.Subtract(minDate);
+                                ChangeDaysQtyTextInAnotherThread(diffResult.Days.ToString());
+                            }
+                        }
+                        
+                        auditCol = null;
+                        wssQuery = null;
+                    }
+
+                }
+
+            });
+
+            //Calculate date difference between now and minDate and assign to label.
+
+            diffResult = DateTime.Now.Subtract(minDate);
+            lblDaysQty.Text = diffResult.Days.ToString();
+            lblDaysQty.ForeColor = Color.Red;
+
+            ChangeToolStripTextInAnotherThread("Ready");
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //load sitecollections unto list
+            lstSiteCollections.Items.Clear();
+
+            SPSecurity.RunWithElevatedPrivileges(delegate
+            {
+
+                SPServiceCollection services = SPFarm.Local.Services;
+                foreach (SPService curService in services)
+                {
+                    if (curService is SPWebService)
+                    {
+                        SPWebService webService = (SPWebService)curService;
+
+                        foreach (SPWebApplication webApp in webService.WebApplications)
+                        {
+                            foreach (SPSite sc in webApp.Sites)
+                            {
+                                try
+                                {
+                                    lstSiteCollections.Items.Add(sc.Url);
+                                }
+                                catch (Exception ex)
+                                {
+                                    //Console.WriteLine("Exception occured: {0}\r\n{1}", ex.Message, ex.StackTrace);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            });
+
+            ChangeToolStripTextInAnotherThread("Ready");
+        }
+    }
+}
